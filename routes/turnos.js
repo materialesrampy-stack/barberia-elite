5009f0f09ae2f1f048ef5ef5fc3e192c79c0ac92
materialesrampy-stack <materialesrampy@gmail.com>
2026-04-10 16:34:33 -0300

    fix: static frontend path

--- routes/turnos.js
+++ routes/turnos.js
@@ -13,7 +13,8 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  family: 4
 });
 
 function verificarToken(req, res) {
