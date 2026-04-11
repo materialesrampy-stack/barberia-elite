0eb8dad87d03deda828d7843443f19aa76fd7bb7
materialesrampy-stack <materialesrampy@gmail.com>
2026-04-11 20:01:56 -0300

    primer commit

--- frontend/script.js
+++ frontend/script.js
@@ -166,5 +166,12 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     });
 
+    const toggle = document.getElementById("menu-toggle");
+    const nav = document.querySelector(".nav-links");
+
+    toggle.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+
 
 });
